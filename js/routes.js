6e5dc2890caedc4cef31e7d91de4bef9598872b5
Gerardo Crot Rojas <gicrot@uc.cl>
2022-05-08 17:50:29 -0400

    feat: show inputs & notifications :sparkles:

--- js/routes.js
+++ js/routes.js
@@ -1,9 +1,9 @@
-export const post_tweet = (user, tweet, token) =>{
-    fetch('http://localhost:5000/tweets/post',{
+export const post_tweet = async (user, tweet, token) =>{
+    fetch('http://localhost:3000/tweets/post',{
         method: 'post',
         body: JSON.stringify({
             user: user,
-            tweet:tweet,
+            tweet: tweet,
             token: token
         }),
         headers: {
@@ -11,21 +11,8 @@ export const post_tweet = (user, tweet, token) =>{
             'Content-Type': 'application/json'
         }
     }).then(response =>{
-        fetch('./index.html').then(response =>{
-            response.text().then(data=>{
-                
-
-            })
-        }
-        )    
+        fetch('./index.html').then(response => response.text())    
     })
     .catch((err)=>{
         console.log('An error occurred while retrieving token. ', err);
     })};
-
-
-
- 
-
-
-
